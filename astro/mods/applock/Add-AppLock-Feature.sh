d962c5336340ab1f546912fe2ca834d7ef4ba926
mehedihjoy0 <mh506370@gmail.com>
2026-08-16 12:37:20 +0600

    astro/mods/applock: init

---
+++ astro/mods/applock/Add-AppLock-Feature.sh
@@ -0,0 +1,4 @@
+LOG_BEGIN "- Adding AppLock support from e1q"
+ADD_FROM_FW "e1q" "system" "priv-app/AppLock/AppLock.apk"
+ADD_FROM_FW "e1q" "system" "etc/permissions/privapp-permissions-com.samsung.android.applock.xml"
+LOG_END
